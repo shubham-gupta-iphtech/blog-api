4ec29ed757a42e75510e9765946d7411e5b9feed
Shubham Gupta <shubham.gupta@iphtechnologies.com>
2025-04-17 11:30:45 +0530

    changed the user schema

--- models/Blog.js
+++ models/Blog.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const blogSchema = new mongoose.Schema({
   title: String,
   content: String,
+  category :{type: String, enum: ['generic', 'travel', 'fooding', 'educational'], default: 'generic'},
   coverImage: {type: String , default: ""},
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
